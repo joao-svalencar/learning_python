08bc4d4416dbf3b67c7ff501c2028e3780611c07
João Paulo Alencar <66645449+joao-svalencar@users.noreply.github.com>
2021-03-01 20:22:25 -0300

    new commit

---
+++ Desafio 3.2.py
@@ -0,0 +1,8 @@
+print('====== DESAFIO 3 ======')
+num1 = int(input('Primeiro número'))
+num2 = int(input('Segundo número'))
+soma = num1 + num2
+#print('A soma é', soma)
+#outra forma de representar isso é:
+
+print('A soma é {}'.format(soma))
