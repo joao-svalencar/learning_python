fd554bbdd2a0aa645709b614aeba63f1ffcdb9ab
João Paulo Alencar <joaosvalencar@Joaos-MacBook-Pro.local>
2022-05-31 12:13:10 -0300

    creatingFiles

--- creatingData.R
+++ creatingData.R
@@ -4,4 +4,6 @@ pass <- sample(c("P", "F"), 26, replace = TRUE)
 
 input <- data.frame(names, age, pass)
 input
+colnames(input) <- NULL
 
+write.table(input, 'inputFile.txt', sep="\t", row.names = FALSE)
